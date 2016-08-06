dcf093357b4a7d1b06826cc1eec7aef79ff34165
James Dempsey <james.dempsey@gmail.com>
2016-08-06 17:11:31 +1000

    Add signal-to-noise ratio to image comparison page

--- image-1420.py
+++ image-1420.py
@@ -2,6 +2,7 @@
 # 1757 MHz continuum data
 
 import aplpy
+import math
 import os
 import process_data
 import sys
@@ -33,12 +34,19 @@ t = Template('<html>\n<head><title>Image previews for day $day</title></head>\n'
              + '<tr><td>Source</td><td>1757 MHz</td><td>1420 MHz</td>')
 img_idx.write(t.substitute(day=day))
 
-freq='1420'
 os.chdir(dayDirName)
 for src in sources:
     src_name = src['source']
     name_prefix = '/magmo-' + src_name + '_'
-    img_idx.write('<tr><td><br>'+src_name+'</td>\n')
+    restored_img = "1757/magmo-" + src_name + "_1757_restor"
+    sn = 0.0
+    if os.path.exists(restored_img):
+        rms, max = process_data.get_signal_noise_ratio(restored_img)
+        if rms > 0:
+            sn = max / rms
+        sn /= math.sqrt(1053)
+
+    img_idx.write('<tr><td>' + src_name + '<br>S/N: ' + str(sn) + '</td>\n')
     for freq in ['1757', '1420']:
         fits_file = freq + name_prefix + freq + '_restor.fits'
         img = freq + name_prefix + freq + '.png'
