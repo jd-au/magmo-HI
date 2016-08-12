1c25ed8909ddae8b818bd544a04626dba7ff3e56
James Dempsey <james.dempsey@gmail.com>
2016-08-13 09:34:31 +1000

    Add beam image to diagnostic plots

--- image-1420.py
+++ image-1420.py
@@ -2,6 +2,7 @@
 # 1757 MHz continuum data
 
 import aplpy
+import magmo
 import math
 import os
 import process_data
@@ -16,7 +17,7 @@ if len(sys.argv) != 2:
     exit(1)
 day = sys.argv[1]
 
-sources = process_data.get_day_obs_data(day)
+sources = magmo.get_day_obs_data(day)
 if sources is None or len(sources) == 0:
     print "Day %s is not defined." % (day)
     exit(1)
@@ -31,7 +32,7 @@ error_list.extend(process_data.build_images(dayDirName, sources, line_band, day)
 img_idx = open(dayDirName + '/images-comp.html', 'w')
 t = Template('<html>\n<head><title>Image previews for day $day</title></head>\n'
              + '<body>\n<h1>Image previews for day $day</h1>\n<table>\n'
-             + '<tr><td>Source</td><td>1757 MHz</td><td>1420 MHz</td>')
+             + '<tr><td>Source</td><td>1757 MHz</td><td>1420 MHz</td><td>1757 Beam</td></tr>')
 img_idx.write(t.substitute(day=day))
 
 os.chdir(dayDirName)
@@ -46,7 +47,15 @@ for src in sources:
             sn = max / rms
         sn /= math.sqrt(1053)
 
-    img_idx.write('<tr><td>' + src_name + '<br>S/N: ' + str(sn) + '<br>Max: ' + str(max) + '</td>\n')
+    beam_file = "1757/magmo-" + src_name + "_1757_beam"
+    beam_img = beam_file + ".png"
+    if os.path.exists(beam_file):
+        cmd = 'cgdisp region="percentage(15)" in=' + beam_file + ' device=' + beam_img\
+              + '/png type=p'
+        magmo.run_os_cmd(cmd)
+
+    img_idx.write('<tr><td>' + src_name + '<br>S/N: ' + str(sn) + '<br>Max: ' + str(max)
+                  + '<br>RMS: ' + str(rms) + '</td>\n')
     for freq in ['1757', '1420']:
         fits_file = freq + name_prefix + freq + '_restor.fits'
         img = freq + name_prefix + freq + '.png'
@@ -62,6 +71,8 @@ for src in sources:
         t = Template('<td><a href="${img}"><img src="${img}" width="500px"></a></td>')
         img_idx.write(t.substitute(img=img))
 
+    t = Template('<td><a href="${img}"><img src="${img}" width="500px"></a></td>')
+    img_idx.write(t.substitute(img=beam_img))
     img_idx.write('</tr>\n')
 
 img_idx.write('</table></body></html>\n')
