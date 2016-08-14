d431ef07e9c639f1664fc7ba4c98894e8704a3fb
James Dempsey <james.dempsey@gmail.com>
2016-08-14 22:15:43 +1000

    Add day number parameter to archive.sh

--- archive.sh
+++ archive.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-daydir='day21'
+day=$1
+daydir="day${day}"
 today=`date +%Y%m%d`
 archivedir="prev_runs/${daydir}-${today}"
 # Copy all
 mkdir ${archivedir}
 mkdir  ${archivedir}/1420
 mkdir  ${archivedir}/1757
-cp day21/*.html ${archivedir}/
-cp day21/*.log ${archivedir}/
-cp day21/*.png* ${archivedir}/
-cp day21/1420/*.png* ${archivedir}/1420/
-cp day21/1757/*.png* ${archivedir}/1757/
+cp ${daydir}/*.html ${archivedir}/
+cp ${daydir}/*.log ${archivedir}/
+cp ${daydir}/*.png* ${archivedir}/
+cp ${daydir}/1420/*.png* ${archivedir}/1420/
+cp ${daydir}/1757/*.png* ${archivedir}/1757/
