0eb629cfb46ee87215ad27806ee094317c26bb4f
James Dempsey <james.dempsey@csiro.au>
2016-09-07 07:25:38 +1000

    Earlier restructure

--- prototypes/get_spectrum.py
+++ prototypes/get_spectrum.py
@@ -4,9 +4,11 @@
 
 from astropy.io import fits
 from astropy.io import votable
+from astropy.coordinates import SkyCoord
 from astropy.wcs import WCS
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.core.records as rec
 
 
 def readSources(filename):
@@ -60,49 +62,45 @@ def plotSpectrum(x, y):
     return
 
 
+def extract_spectra(daydirname, field):
+    fits_filename = "{0}/1420/magmo-{1}_1420_sl_restor.fits".format(daydirname,
+                                                                    field)
+    src_filename = "{0}/{1}_src_comp.vot".format(daydirname, field)
+
+    spectra = dict()
+    sources = readSources(src_filename)
+    hdulist = fits.open(fits_filename)
+    image = hdulist[0].data
+    header = hdulist[0].header
+    w = WCS(header)
+    index = np.arange(header['NAXIS3'])
+    velocities = w.wcs_pix2world(10,10,index[:],0,0)[2]
+    for src in sources:
+        pix = w.wcs_world2pix(src[0], src[1], 0, 0, 1)
+        x_coord = int(round(pix[0])) - 1  # 266
+        y_coord = int(round(pix[1])) - 1  # 197
+        print "Translated %.4f, %.4f to %d, %d" % (
+        src[0], src[1], x_coord, y_coord)
+
+        slice = image[0, :, y_coord, x_coord]
+
+        plotSpectrum(np.arange(slice.size), slice)
+        spectrum_array = rec.fromarrays([np.arange(slice.size), velocities, \
+                                        slice], names='plane,velocity,flux')
+        c = SkyCoord(src[0], src[1], frame='icrs', unit="deg")
+        spectra[c.galactic.l] = spectrum_array
+    del image
+    del header
+    hdulist.close()
+
+    return spectra
+
 #fits_filename = 'day21/1420/magmo-270.255+0.835_1420_sl_restor.fits'
 #fits_filename = 'day21/1420/magmo-291.270-0.719_1420_sl_restor.fits'
 field = '285.337-0.002'
 daydirname = 'day21'
-fits_filename = "{0}/1420/magmo-{1}_1420_sl_restor.fits".format(daydirname, field)
-src_filename = "{0}/{1}_src_comp.vot".format(daydirname, field)
-ra = 157.87033137328115
-dec = -58.04001286446428
+spectra_dict = extract_spectra(daydirname, field)
+print spectra_dict
 
-
-#sources = readSources('day21/285.337-0.002_src_comp.vot')
-sources = readSources(src_filename)
-hdulist_im = fits.open(fits_filename)
-image = hdulist_im[0].data
-header = hdulist_im[0].header
-#header = fits.getheader(fits_filename)
-w = WCS(header)
-for src in sources:
-    pix = w.wcs_world2pix(src[0], src[1], 0, 0, 1)
-    x_coord = int(round(pix[0]))-1 # 266
-    y_coord = int(round(pix[1]))-1 # 197
-    print "Translated %.4f, %.4f to %d, %d" % (src[0], src[1], x_coord, y_coord)
-
-    slice = image[0,:,y_coord, x_coord]
-
-    plotSpectrum(np.arange(slice.size), slice)
-
-x_size = image.shape[2]
-y_size = image.shape[3]
-continuum = np.zeros((x_size, y_size))
-
-for i in range(0,x_size-1):
-    for j in range(0,y_size-1):
-        slice = image[0,28:228,i,j]
-        continuum[i,j] = max(0,np.mean(slice))
-        if continuum[i,j] > 0.1:
-            print i, j, continuum[i,j]
-            if i > 1 and j > 1:
-                slice = image[0, :, i, j]
-                #plotSpectrum(np.arange(slice.size), slice)
-
-plt.imshow(continuum, cmap='gray')
-plt.colorbar()
-plt.show()
 
 
