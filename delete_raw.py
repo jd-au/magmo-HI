9edcdbe9d4a058fc0d02fd17925192e130721217
James Dempsey <james.dempsey@csiro.au>
2016-09-07 07:37:00 +1000

    Complete initial version

--- delete_raw.py
+++ delete_raw.py
@@ -42,64 +42,25 @@ def main():
     if dayRow is None:
         print "Day %s is not defined." % (day)
         exit(1)
-    print "#### Started loading MAGMO day %s at %s ####" % \
+    print "#### Started deleting raw data for MAGMO day %s at %s ####" % \
           (day, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start)))
+
+    print dayRow
+
+    # Delete files
+    startDatePatterns = 2
+    for i in range(startDatePatterns, len(dayRow)):
+        os.system("rm -r rawdata/" + dayRow[i] + "* ")
+
+    # Report
+    end = time.time()
+    print '#### Deleting completed at %s ####' \
+          % (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end)))
+    print 'Processed in %.02f s' % (end - start)
 
     return 0
 
-# ### Script starts here ###
-print dayRow
 
-# Make day directory
-dayDirName = "day" + day
-magmo.ensure_dir_exists(dayDirName)
-
-# Load files
-freqList = ["1421", "1757"]
-ifList = [7, 1]
-restFreqList = ["1.420405752", "1.757"]
-inFilePatterns = ""
-startDatePatterns = 2
-for i in range(startDatePatterns, len(dayRow)):
-    inFilePatterns += "rawdata/" + dayRow[i] + "* "
-
-for i in range(0, len(freqList)):
-    freq = freqList[i]
-    ifsel = ifList[i]
-    restFreq = restFreqList[i]
-    load_rpfits(freq, ifsel, restFreq, dayRow[0], inFilePatterns)
-
-# Split files
-os.chdir("day" + dayRow[0])
-for freq in freqList:
-    uvFile = "MAGMO_day" + dayRow[0] + "_" + freq + ".uv"
-    uvsplitCmd = "uvsplit vis=" + uvFile
-    magmo.run_os_cmd(uvsplitCmd)
-# Rename any 1420.5 folders to remove the decimal
-uv_dirs = glob.glob('*.[0-9][0-9][0-9][0-9].[0-9]')
-for uvdir in uv_dirs:
-    os.rename(uvdir, uvdir[:-2])
-os.chdir("..")
-
-
-# Backup
-backupDirName = dayDirName+"/backup"
-magmo.ensure_dir_exists(backupDirName)
-for uvDir in os.listdir(dayDirName):
-    uvDirName = dayDirName + "/" + uvDir
-    if (not uvDirName.endswith("backup")) and (not uvDirName.endswith(".uv")) and os.path.isdir(uvDirName):
-        uvBackupDir = backupDirName + "/" + uvDir
-        magmo.ensure_dir_exists(uvBackupDir)
-        for name in ['flags', 'header', 'history']:
-            shutil.copy2(uvDirName + "/" + name, uvBackupDir)
-
-# Cleanup
-#for freq in freqList:
-#    cmd = 'rm -rf day' + dayRow[0] + '/MAGMO_day' + dayRow[0] + '_' + freq + '.uv'
-#    magmo.run_os_cmd(cmd)
-
-# Report
-end = time.time()
-print '#### Loading completed at %s ####' \
-      % (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end)))
-print 'Processed in %.02f s' % (end - start)
+# Run the script if it is called from the command line
+if __name__ == "__main__":
+    exit(main())
