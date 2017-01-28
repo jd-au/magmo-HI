623199855d8482a3ddd5820c1c91e65868208e8c
James Dempsey <james.dempsey@csiro.au>
2017-01-29 08:28:20 +1100

    Add support for two phase GaussPy processing Use 'conv' mode Skip single problematic spectrum Remove training code Add fit information to catalogue Plot all fits

--- decompose.py
+++ decompose.py
@@ -13,16 +13,18 @@ from __future__ import print_function, division
 from astropy.io import fits, votable
 from astropy.io.votable import parse, from_table, writeto
 from astropy.table import Table, Column
+from matplotlib import gridspec
 from string import Template
 
 import argparse
 import datetime
 import gausspy.gp as gp
+import magmo
 import numpy as np
 import pickle
 import time
 import matplotlib.pyplot as plt
-
+import aplpy
 
 FILENAME_DATA_GAUSSPY = 'spectra.pickle'
 
@@ -46,10 +48,12 @@ def parseargs():
                         action='store_true')
     parser.add_argument("--train", help="Train GaussPy using the selected spectra. The produced alpha value can then " +
                                         "be used for later decompostion.", default=False, action='store_true')
-    parser.add_argument("--alpha", help="The value for the GaussPy smoothing parameter",
+    parser.add_argument("--alpha1", help="The value for the first GaussPy smoothing parameter",
                         type=float, default=1.12)
+    parser.add_argument("--alpha2", help="The value for the second GaussPy smoothing parameter",
+                        type=float, default=5)
     parser.add_argument("--snr_thresh", help="The signal to noise ratio threshold",
-                        type=float, default=0.2)
+                        type=float, default=5)
 
     args = parser.parse_args()
     return args
@@ -83,34 +87,66 @@ def get_opacity_filename(day, field, source):
     return t.substitute(day=day, field=field, source=source)
 
 
+def convert_from_ratio(absorption):
+    """
+    Convert an array of absorption values (I/I_0) to opacity values (tau).
+    Note that this currently clips any negative values to avoid nan in the
+    result.
+    :param absorption: The array of absorption values
+    :return: The equivalent tau values
+    """
+    #return -1 * np.log(np.maximum(absorption, 1e-16))
+    return 1 - absorption
+
+
+def convert_to_ratio(opacity):
+    """
+    Convert an array of opacity values (tau) to absorption values (I/I_0).
+    :param opacity: The array of tau values
+    :return: The equivalent absorption ratio values
+    """
+    #return np.exp(-1 * opacity)
+    return 1 - opacity
+
+
 def prepare_spectra(spectra):
     data = {}
-
-    # TODO: Convert this to use a real error value
-    rms = 0.06
 
     # Convert to GaussPy format
     i = 0
     for spectrum in spectra:
         filename = get_opacity_filename(spectrum['Day'], spectrum['Field'], spectrum['Source'])
-        print ("Reading", filename)
+        #print("Reading", filename)
         opacity = read_opacity(filename)
+        if spectrum['Day'] == 43 and spectrum['Rating'] == 'A' and spectrum['Field'] == '019.612-0.120': # and spectrum['Source'] == '25-0':
+            print("Skipping ", spectrum['Rating'], spectrum['Day'], spectrum['Longitude'], spectrum['Field'], spectrum['Source'])
+            continue
+
+        longitude = spectrum['Longitude']
+        if longitude < 0:
+            longitude += 360
+        print (i, spectrum['Rating'], spectrum['Day'], longitude, spectrum['Field'], spectrum['Source'])
+        rms = spectrum['Continuum_SD']
 
         errors = np.ones(opacity.shape[0]) * rms
         location = np.array(spectrum['Longitude'], spectrum['Latitude'])
-        inverted_opacity= 1- opacity['opacity']
+        tau = convert_from_ratio(opacity['opacity'])
+        #print (tau)
+        #inverted_opacity = 1 - opacity['opacity']
 
-        data['data_list'] = data.get('data_list', []) + [inverted_opacity]
-        data['x_values'] = data.get('x_values', []) + [opacity['velocity']/1000]
+        data['data_list'] = data.get('data_list', []) + [tau]
+        data['x_values'] = data.get('x_values', []) + [opacity['velocity'] / 1000]
         data['errors'] = data.get('errors', []) + [errors]
         data['location'] = data.get('location', []) + [location]
-        i+= 1
+        data['spectrum_idx'] = data.get('spectrum_idx', []) + [i]
+        data['rating'] = data.get('rating', []) + [spectrum['Rating']]
+        i += 1
 
     # Save the file to be used by GaussPy
     pickle.dump(data, open(FILENAME_DATA_GAUSSPY, 'w'))
 
 
-def decompose(spectra, out_filename, alpha1, snr_thresh):
+def decompose(spectra, out_filename, alpha1, alpha2, snr_thresh):
     start = time.time()
     print("## Commenced decomposition at %s ##" %
           time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start)))
@@ -125,9 +161,13 @@ def decompose(spectra, out_filename, alpha1, snr_thresh):
     g = gp.GaussianDecomposer()
 
     # Setting AGD parameters
-    g.set('phase', 'one')
+    g.set('mode','conv')
+    g.set('phase', 'two' if alpha2 else 'one')
     g.set('SNR_thresh', [snr_thresh, snr_thresh])
     g.set('alpha1', alpha1)
+    #g.set('verbose', True)
+    if alpha2:
+        g.set('alpha2', alpha2)
 
     # Run GaussPy
     decomposed_data = g.batch_decomposition(FILENAME_DATA_GAUSSPY)
@@ -140,33 +180,7 @@ def decompose(spectra, out_filename, alpha1, snr_thresh):
           (len(spectra), time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end)), (end - end_read)))
 
 
-def train(spectra, alpha1_initial, snr_thresh):
-    start = time.time()
-    print("## Commenced training at %s ##" %
-          time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start)))
-
-    prepare_spectra(spectra)
-
-    end_read = time.time()
-    print("## Finished conversion of %d spectra at %s taking %.02f s ##" %
-          (len(spectra), time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_read)), (end_read - start)))
-
-    g = gp.GaussianDecomposer()
-    # Next, load the training dataset for analysis:
-    g.load_training_data(FILENAME_DATA_GAUSSPY)
-    # Set GaussPy parameters
-    g.set('phase', 'one')
-    g.set('SNR_thresh', [snr_thresh, snr_thresh])
-
-    # Train AGD starting with initial guess for alpha
-    g.train(alpha1_initial=alpha1_initial)
-
-    end = time.time()
-    print("## Finished training using %d spectra at %s taking %.02f s ##" %
-          (len(spectra), time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end)), (end - end_read)))
-
-
-def output_component_catalogue(spectra, data_decomposed):
+def output_component_catalogue(spectra, data, data_decomposed):
     days = []
     field_names = []
     sources = []
@@ -175,14 +189,24 @@ def output_component_catalogue(spectra, data_decomposed):
     amps = []
     fwhms = []
     means = []
+    best_fit_rchi2s = []
+    amps_fit_errs = []
+    fwhms_fit_errs = []
+    means_fit_errs = []
+
 
     num_no_comps = {}
 
     for i in range(len(data_decomposed['fwhms_fit'])):
-        spectrum = spectra[i]
+        spectrum = spectra[data['spectrum_idx'][i]]
         fit_fwhms = data_decomposed['fwhms_fit'][i]
         fit_means = data_decomposed['means_fit'][i]
         fit_amps = data_decomposed['amplitudes_fit'][i]
+        best_fit_rchi2 = data_decomposed['best_fit_rchi2'][i]
+        means_fit_err = data_decomposed['means_fit_err'][i]
+        fwhms_fit_err = data_decomposed['fwhms_fit_err'][i]
+        amplitudes_fit_err = data_decomposed['amplitudes_fit_err'][i]
+
         if len(fit_amps) > 0.:
             for j in range(len(fit_amps)):
                 days.append(int(spectrum['Day']))
@@ -193,76 +217,177 @@ def output_component_catalogue(spectra, data_decomposed):
                 amps.append(fit_amps[j])
                 fwhms.append(fit_fwhms[j])
                 means.append(fit_means[j])
+                best_fit_rchi2s.append(best_fit_rchi2[0])
+                amps_fit_errs.append(means_fit_err[j])
+                fwhms_fit_errs.append(fwhms_fit_err[j])
+                means_fit_errs.append(amplitudes_fit_err[j])
         else:
             rating = spectrum['Rating']
             num_no_comps[rating] = num_no_comps.get(rating, 0) + 1
 
-
     temp_table = Table(
-        [days, field_names, sources, longitudes, latitudes, amps, fwhms, means],
-        names=['Day', 'Field', 'Source', 'Longitude', 'Latitude', 'Amplitude', 'FWHM', 'Mean'],
+        [days, field_names, sources, longitudes, latitudes, amps, fwhms, means, best_fit_rchi2s, amps_fit_errs,
+         fwhms_fit_errs, means_fit_errs],
+        names=['Day', 'Field', 'Source', 'Longitude', 'Latitude', 'Amplitude', 'FWHM', 'Mean', 'Best Fit Rchi2',
+               'Amplitude Fit Err', 'FWHM Fit Err', 'Mean Fit Err'],
         meta={'ID': 'magmo_components',
               'name': 'MAGMO Components ' + str(datetime.date.today())})
     votable = from_table(temp_table)
     filename = "magmo-components.vot"
     writeto(votable, filename)
 
-    print("Wrote out", len(fwhms), "components to", filename, "No components generated for")
+    total_nc = 0
+    for rating, count in num_no_comps.items():
+        total_nc += count
+
+    print("Wrote out", len(fwhms), "components to", filename, "No components generated for", total_nc)
     for rating in sorted(num_no_comps.keys()):
-        print ("%s: %3d" % (rating, num_no_comps[rating]))
-    # , ".", num_no_comps, "spectra (of", len(spectra), ") had no components found")
+        print("%s: %3d" % (rating, num_no_comps[rating]))
+        # , ".", num_no_comps, "spectra (of", len(spectra), ") had no components found")
+
+
+def plot_single_spectrum(ax, velo, opacity, fit_amps, fit_fwhms, fit_means, name):
+    g_sum = np.zeros(len(velo))
+    ax.plot(velo, opacity, color='grey')
+    # Plot individual components
+    if len(fit_amps) > 0.:
+        for j in range(len(fit_amps)):
+            amp, fwhm, mean = fit_amps[j], fit_fwhms[j], fit_means[j]
+            yy = amp * np.exp(-4. * np.log(2) * (velo - mean) ** 2 / fwhm ** 2)
+            g_sum += yy
+            #yy = convert_to_ratio(yy)
+            ax.plot(velo, yy, '--', lw=0.5, color='purple')
+    #g_sum = convert_to_ratio(g_sum)
+    ax.plot(velo, g_sum, '-', lw=1.0, color='blue')
+    plt.title(name)
+    residual = opacity - g_sum
+    return residual
+
+
+def plot_spectrum(velo, opacity, fit_amps, fit_fwhms, fit_means, name, filename):
+    fig = plt.figure(figsize=(8, 6))
+    gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1])
+    ax = fig.add_subplot(gs[0])
+
+    y = convert_from_ratio(opacity)
+    residual = plot_single_spectrum(ax, velo, y, fit_amps, fit_fwhms, fit_means, name)
+    ax.set_ylabel('$1 - e^{-\\tau}$')
+
+    # Residual plot
+    ax = fig.add_subplot(gs[1])
+    ax.plot(velo, residual, 'or', markerfacecolor='None', markersize=2, markeredgecolor='blue')
+    ax.grid()
+
+    plt.xlabel('LSR Velocity (km/s)')
+
+    plt.savefig(filename)
+    plt.close()
+
+
+def plot_spectra(spectra, data, data_decomposed):
+    magmo.ensure_dir_exists("plots")
+    for rating in 'ABCDEF':
+        magmo.ensure_dir_exists("plots/" + rating)
+
+    for i in range(len(data_decomposed['fwhms_fit'])):
+        spectrum = spectra[data['spectrum_idx'][i]]
+        velo = data['x_values'][i]
+        #opacity = 1 - data['data_list'][i]
+        opacity = convert_to_ratio(data['data_list'][i])
+        fit_fwhms = data_decomposed['fwhms_fit'][i]
+        fit_means = data_decomposed['means_fit'][i]
+        fit_amps = data_decomposed['amplitudes_fit'][i]
+        rating = spectrum['Rating']
+        field_name = spectrum['Field']
+        day = str(spectrum['Day'])
+        src_id = spectrum['Source']
+        name = field_name + " src " + src_id + " on day " + day + "(" + rating + ")"
+        filename = "plots/" + rating + "/"
+        filename += field_name + "_" + day + "_src" + src_id + "_fit.pdf"
+        plot_spectrum(velo, opacity, fit_amps, fit_fwhms, fit_means, name, filename)
+
+
+def plot_components(spectra, data_decomposed):
+    fits_filename = 'hi4pi-total-car.fits'
+    hdulist = fits.open(fits_filename, memmap=True)
+    image = hdulist[0].data
+    header = hdulist[0].header
+    fig = aplpy.FITSFigure(image, header)
+    fig.set_theme('publication')
+    # fig.show_colorscale(vmin=vmin, vmax=np.max(image), cmap="jet")
+    fig.add_colorbar()
+    fig.save('magmo-map-comp.pdf')
+    fig.close()
+
+
+def get_samples(data, rating_count=[4, 1, 1], ratings='ABC'):
+    indexes = []
+    found_spectra = [0,0,0]
+    print (len(data['rating']))
+    for i in range(len(data['rating'])):
+        print (data['rating'][i])
+        key = ratings.index(data['rating'][i])
+        if key >= 0 and found_spectra[key] < rating_count[key]:
+            indexes.append(i)
+            found_spectra[key] += 1
+    return indexes
 
 
 def output_decomposition(spectra, out_filename):
-
     # For each spectra:
     # Output plots
     # Output component catalogue
 
     max_results = 6 if len(spectra) > 6 else len(spectra)
-    print (max_results)
+    print(max_results)
 
     data = pickle.load(open(FILENAME_DATA_GAUSSPY))
     data_decomposed = pickle.load(open(out_filename))
-    index_values = np.argsort(np.random.randn(5000))  # plot random results
+
+    index_values = get_samples(data)
+    print (index_values, len(data_decomposed['fwhms_fit']), len(spectra))
+
     fig = plt.figure(0, [12, 12])
-    for i in range(max_results):
-        ax = fig.add_subplot(4, 3, i+1+((i//3)*3)) #, sharex=True)
-        index = i #index_values[i]
+    i = 0
+    for index in index_values:
+        ax = fig.add_subplot(4, 3, i + 1 + ((i // 3) * 3))  # , sharex=True)
+        #index = i  # index_values[i]
         x = data['x_values'][index]
-        y = 1-data['data_list'][index]
+        #y = 1 - data['data_list'][index]
+        #y = convert_to_ratio(data['data_list'][index])
+        y = data['data_list'][index]
         fit_fwhms = data_decomposed['fwhms_fit'][index]
         fit_means = data_decomposed['means_fit'][index]
         fit_amps = data_decomposed['amplitudes_fit'][index]
-        g_sum = np.zeros(len(x))
-        ax.plot(x, y, color='grey')
-        # Plot individual components
-        if len(fit_amps) > 0.:
-            for j in range(len(fit_amps)):
-                amp, fwhm, mean = fit_amps[j], fit_fwhms[j], fit_means[j]
-                yy = amp * np.exp(-4. * np.log(2) * (x - mean) ** 2 / fwhm ** 2)
-                g_sum += yy
-                yy = 1- yy
-                ax.plot(x, yy, '-', lw=1.5, color='purple')
 
-        ax.plot(x, 1-g_sum, '--', lw=1.0, color='blue')
+        spectrum = spectra[data['spectrum_idx'][index]]
+        rating = spectrum['Rating']
+        field_name = spectrum['Field']
+        day = str(spectrum['Day'])
+        src_id = spectrum['Source']
+        name = field_name + " src " + src_id + " d " + day + "(" + rating + ")"
+
+        residual = plot_single_spectrum(ax, x, y, fit_amps, fit_fwhms, fit_means, name)
 
         if i % 3 == 0:
             ax.set_ylabel('$1 - e^{-\\tau}$')
 
         # Residual plot
-        residual = y - (1-g_sum)
-        ax = fig.add_subplot(4, 3, i+4+((i//3)*3))
-        #frame2 = ax.add_axes((.1, .1, .8, .2))
+        ax = fig.add_subplot(4, 3, i + 4 + ((i // 3) * 3))
+        # frame2 = ax.add_axes((.1, .1, .8, .2))
         ax.plot(x, residual, 'or', markerfacecolor='None', markersize=2, markeredgecolor='blue')
         ax.grid()
 
-        #ax.set_xlim(400, 600)
+        # ax.set_xlim(400, 600)
         if i >= 3:
             ax.set_xlabel('LSR Velocity (km/s)')
+        i += 1
+
+    plt.tight_layout()
     plt.savefig("magmo-decomp.pdf")
 
-    output_component_catalogue(spectra, data_decomposed)
+    output_component_catalogue(spectra, data, data_decomposed)
+    plot_spectra(spectra, data, data_decomposed)
 
 
 def main():
@@ -278,14 +403,11 @@ def main():
     spectra = filter_spectra(spectra, args.long_min, args.long_max)
 
     # Decompose all spectra
-    if args.train:
-        train(spectra, args.alpha, args.snr_thresh)
-    elif not args.plot_only:
-        decompose(spectra, args.output, args.alpha, args.snr_thresh)
+    if not args.plot_only:
+        decompose(spectra, args.output, args.alpha1, args.alpha2, args.snr_thresh)
 
     # Read in result
-    if not args.train:
-        output_decomposition(spectra, args.output)
+    output_decomposition(spectra, args.output)
 
     # Report
     end = time.time()
